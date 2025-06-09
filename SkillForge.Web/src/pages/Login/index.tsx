aa09d593758aa891fee783f983e1bf8a51fe0316
Nameless10000 <116448914+Nameless10000@users.noreply.github.com>
2025-06-09 23:06:31 +0500

    reg form added
    add reg request logic

--- SkillForge.Web/src/pages/Login/index.tsx
+++ SkillForge.Web/src/pages/Login/index.tsx
@@ -1,7 +1,98 @@
 import { API } from '@/constants';
 import { LoginFormPage, ProFormText } from '@ant-design/pro-components';
 import { request } from '@umijs/max';
+import { Tabs, TabsProps } from 'antd';
+import TabPane from 'antd/es/tabs/TabPane';
 import { AuthedUser, UserLogin } from 'typings';
+
+const pageTabs: TabsProps['items'] = [
+  {
+    key: "login",
+    label: "Login",
+    children: <>
+    <ProFormText
+        name={'username'}
+        required
+        rules={[
+          {
+            required: true,
+            message: 'Enter username',
+          },
+        ]}
+      />
+      <ProFormText.Password
+        name={'password'}
+        required
+        rules={[
+          {
+            required: true,
+            message: 'Enter password',
+          },
+          {
+            validator(_, value, callback) {
+              if ((value as string).length < 6)
+                callback('min password length: 6');
+
+              callback();
+            },
+          },
+        ]}
+      />
+    </>
+  },
+  {
+    key: "reg",
+    label: "Register",
+    children: <>
+    <ProFormText
+        name={'email'}
+        required
+        rules={[
+          {
+            required: true,
+            message: 'Enter email',
+          },
+          {
+            validator(rule, value, callback) {
+                if (!(value as string).includes("@"))
+                  callback("Incorrect email format");
+
+                callback();
+            },
+          }
+        ]}
+      />
+    <ProFormText
+        name={'username'}
+        required
+        rules={[
+          {
+            required: true,
+            message: 'Enter username',
+          },
+        ]}
+      />
+      <ProFormText.Password
+        name={'password'}
+        required
+        rules={[
+          {
+            required: true,
+            message: 'Enter password',
+          },
+          {
+            validator(_, value, callback) {
+              if ((value as string).length < 6)
+                callback('min password length: 6');
+
+              callback();
+            },
+          },
+        ]}
+      />
+    </>
+  }
+]
 
 export default () => {
   const onFormFinish = async (data: UserLogin) => {
@@ -29,34 +120,8 @@ export default () => {
 
   return (
     <LoginFormPage<UserLogin> onFinish={onFormFinish}>
-      <ProFormText
-        name={'username'}
-        required
-        rules={[
-          {
-            required: true,
-            message: 'Enter username',
-          },
-        ]}
-      />
-      <ProFormText.Password
-        name={'password'}
-        required
-        rules={[
-          {
-            required: true,
-            message: 'Enter password',
-          },
-          {
-            validator(_, value, callback) {
-              if ((value as string).length < 6)
-                callback("min password length: 6");
-
-              callback();
-            },
-          },
-        ]}
-      />
+        <Tabs items={pageTabs}  defaultActiveKey='login'/>
+      
     </LoginFormPage>
   );
 };
